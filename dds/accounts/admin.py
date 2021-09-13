0f633d59bf46f27fc5e10452f5f7a3d39963ae38
Vladimir Kuratov <kuratovv29@gmail.com>
2021-09-13 14:42:05 +0300

    default avatars load from admin panel

--- dds/accounts/admin.py
+++ dds/accounts/admin.py
@@ -1,8 +1,35 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from dds.accounts.models import AdvUser, VerificationForm, MasterUser
+from dds.accounts.models import AdvUser, VerificationForm, MasterUser, DefaultAvatar
 from allauth.socialaccount.models import SocialToken, SocialAccount, SocialApp
 from allauth.account.models import EmailAddress
+
+from dds.store.services.ipfs import send_to_ipfs
+
+
+class DefaultAvatarForm(forms.ModelForm):
+    avatar = forms.ImageField()
+
+    def save(self, commit=True):
+        avatar = self.cleaned_data.get('avatar', None)
+        image = send_to_ipfs(avatar)
+        self.instance.image = image
+        return super(DefaultAvatarForm, self).save(commit=commit)
+
+    class Meta:
+        model = DefaultAvatar
+        fields = "__all__"
+
+
+class DefaultAvatarAdmin(admin.ModelAdmin):
+    form = DefaultAvatarForm
+    fieldsets = (
+        (None, {
+            'fields': ('avatar', 'image', ),
+        }),
+    )
+
 
 class VerificationInline(admin.StackedInline):
     model = VerificationForm
@@ -23,6 +50,7 @@ class MasterUserAdmin(admin.ModelAdmin):
 
 admin.site.register(MasterUser, MasterUserAdmin)
 admin.site.register(AdvUser, AdvUserAdmin)
+admin.site.register(DefaultAvatar, DefaultAvatarAdmin)
 admin.site.unregister(Group)
 admin.site.unregister(SocialToken)
 admin.site.unregister(SocialAccount)
