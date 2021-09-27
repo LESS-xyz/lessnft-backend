845bf026bdd7ccffb8c57ddd4afd23c2db1912cd
Vladimir Kuratov <kuratovv29@gmail.com>
2021-09-27 10:49:50 +0300

    refactor update top users stat

--- dds/activity/services/top_users.py
+++ dds/activity/services/top_users.py
@@ -1,4 +1,3 @@
-from itertools import groupby
 from django.utils import timezone
 from datetime import timedelta
 from django.db.models import Sum, F
@@ -15,30 +14,26 @@ PERIODS = {
 }
 
 
-def _update_users_stat(users, type_):
-    for user in users:
-        user_stat, _ = UserStat.objects.get_or_create(user=user)
-
-        for period, time_delta in PERIODS.items():
-            filter_data = {
-                "date__gte": time_delta, 
-                "method": "Buy", 
-                type_: user,
-            }
-            buyer_history = TokenHistory.objects.filter(**filter_data).annotate(
-                user=F('new_owner'), 
-                price=F('usd_price'),
-            ).values('user').annotate(price=Sum('price'))
-            user_stat.buyer[period] = history.get('price')
-
-        user_stat.save()
-
-
 def update_users_stat():
-    buyers = AdvUser.objects.filter(new_owner__method="Buy").distinct()
-    sellers = AdvUser.objects.filter(old_owner__method="Buy").distinct()
-    _update_users_stat(buyers, 'new_owner')
-    _update_users_stat(sellers, 'old_owner')
+    for type_ in ["new_owner", "old_owner"]:
+        user_filter = {f"{type_}__method": "Buy"}
+        users = AdvUser.objects.filter(**user_filter).distinct()
+        for user in users:
+            user_stat, _ = UserStat.objects.get_or_create(user=user)
+
+            for period, time_delta in PERIODS.items():
+                filter_data = {
+                    "date__gte": time_delta, 
+                    "method": "Buy", 
+                    type_: user,
+                }
+                history = TokenHistory.objects.filter(**filter_data).annotate(
+                    user=F(type_), 
+                    price=F('usd_price'),
+                ).values('user').annotate(price=Sum('price'))
+                getattr(user_stat, type_)[period] = history.get('price')
+
+            user_stat.save()
 
 
 def get_top_users(type_, period):
