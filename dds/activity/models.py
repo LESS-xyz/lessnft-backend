6c3f247428736a195b3ea3dcbab9a737646698b6
Vladimir Kuratov <kuratovv29@gmail.com>
2021-09-27 12:47:53 +0300

    add usd_price to token_history

--- dds/activity/models.py
+++ dds/activity/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from dds.rates.api import calculate_amount
 
 from dds.consts import MAX_AMOUNT_LEN
 
@@ -64,7 +66,22 @@ class TokenHistory(models.Model):
         related_name='old_owner'
     )
     price = models.DecimalField(max_digits=MAX_AMOUNT_LEN, decimal_places=18, default=None, blank=True, null=True)
+    USD_price = models.DecimalField(max_digits=18, decimal_places=2, default=None, blank=True, null=True)
     is_viewed = models.BooleanField(default=False)
+
+
+def token_history_dispatcher(sender, instance, created, **kwargs):
+    if instance.price and instance.token:
+        instance.USD_price = calculate_amount(
+            instance.price, 
+            instance.token.currency.symbol,
+        )[0]
+        post_save.disconnect(token_history_dispatcher, sender=sender)
+        instance.save(update_fields=['USD_price', ])
+        post_save.connect(token_history_dispatcher, sender=sender)
+
+
+post_save.connect(token_history_dispatcher, sender=TokenHistory)
 
 
 class ListingHistory(models.Model):
