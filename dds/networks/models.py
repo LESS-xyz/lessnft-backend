a9b97a64f6940a0e6249038cb31ae572fef2e14b
dsahdr <dshadr@mail.ru>
2021-10-11 11:39:17 +0300

    template for executing contract interactions through Network model

--- dds/networks/models.py
+++ dds/networks/models.py
@@ -24,6 +24,10 @@ class Network(models.Model):
     Represent different networks as different blockchains, 
     in witch we have our contracts.
     """
+    class Types(models.TextChoices):
+        ethereum = 'ethereum'
+        tron = 'tron'
+
     name = models.CharField(max_length=100)
     needs_middleware = models.BooleanField(default=False)
     native_symbol = models.CharField(max_length=10, blank=True, null=True, default=None)
@@ -31,6 +35,10 @@ class Network(models.Model):
     fabric721_address = models.CharField(max_length=128)
     fabric1155_address = models.CharField(max_length=128)
     exchange_address = models.CharField(max_length=128)
+    network_type = models.CharField(
+        choices=Types.choices,
+        default=Types.ethereum
+    )
 
     def __str__(self):
         return self.name
@@ -65,3 +73,39 @@ class Network(models.Model):
 
     def get_token_contract(self, address: str = None) -> ("Web3", "Contract"):
         return self._get_contract_by_abi(WETH_ABI, address)
+
+    def contract_call(self, method_type: str, **kwargs) -> "result":
+        """
+        redirects to ethereum/tron/whatever_shit_we_will_add_later read/write method
+        kwargs example for ethereum/tron read method:
+        {
+        address: str, #address of contract if necessary
+        contract_type: str, #contract type for calling web3 instance via get_{contract_type}_method()
+        function_name: str, #function name for function selector
+        input_types: tuple, #tuple of function param types (i.e. ('address', 'uint256')) for stupid tron
+        input_params: tuple, #tuple of function param values
+        output_types: tuple, #tuple of output param types if necessary (for stupid tron)
+        }
+        """
+        return getattr(self, f'execute_{self.network_type}_{method_type}_function')(kwargs)
+
+    def execute_ethereum_read_method(self, **kwargs) -> 'result':
+        contract_type = kwargs.get('contract_type')
+        address = kwargs.get('address')
+        function_name = kwargs.get('function_name')
+        input_params = kwargs.get('input_params')
+        #don't like this if-else , TODO refactor
+        if contract_type in ('exchange', 'erc721fabric', 'erc1155fabric'):
+            _, contract = getattr(self, f'get_{contract_type}_contract')()
+        else:
+            _, contract = getattr(self, f'get_{contract_type}_contract')(address)
+        return getattr(contract.functions, function_name)(*input_params).call()
+
+    def execute_ethereum_write_method(self, **kwargs) -> 'initial_tx':
+        pass
+
+    def execute_tron_read_method(self, **kwargs) -> 'result':
+        pass
+
+    def execute_tron_write_method(self, **kwargs) -> 'initial_tx':
+        pass
