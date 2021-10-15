aea9a340771865883e5a1086d065fd0b37583d4c
Kagiyama <sabitsukimaid@gmail.com>
2021-10-15 01:44:29 -0700

    Necessary periods are now returned by a special func

--- dds/activity/services/top_users.py
+++ dds/activity/services/top_users.py
@@ -5,22 +5,15 @@ from django.db.models import Sum, F, Count
 from dds.accounts.models import AdvUser
 from dds.activity.models import TokenHistory, UserStat, UserAction
 from dds.rates.api import calculate_amount
-from dds.settings import PERIODS
+from dds.utilities import get_periods
 
-# TODO: mb get from config
-'''
-PERIODS = {
-    'day': timezone.now() - timedelta(days=1),
-    'week': timezone.now() - timedelta(days=7),
-    'month': timezone.now() - timedelta(days=30),
-}
-'''
 
 def update_users_stat(network):
     types = {
         'new_owner': 'buyer',
         'old_owner': 'seller',
     }
+    PERIOD = get_periods('day', 'week', 'month')
     for type_ in types.keys():
         user_filter = {f"{type_}__method": "Buy"}
         users = AdvUser.objects.filter(**user_filter).distinct()
@@ -32,44 +25,41 @@ def update_users_stat(network):
                 stat = json.loads(stat)
             else:
                 stat = dict()
-            for period, time_delta in PERIODS.items():
-                if period != 'year':
 
-                    filter_data = {
-                        "token__currency__network": network,
-                        "date__gte": time_delta,
-                        "method": "Buy",
-                        type_: user,
-                    }
-                    history = TokenHistory.objects.filter(**filter_data).annotate(
-                        user=F(type_),
-                        price_=F('USD_price'),
-                    ).values('user').annotate(price=Sum('price_'))
-                    if history:
-                        stat[period] = history[0].get('price')
+            for period, time_delta in PERIOD.items():
+                filter_data = {
+                    "token__currency__network": network,
+                    "date__gte": time_delta,
+                    "method": "Buy",
+                    type_: user,
+                }
+                history = TokenHistory.objects.filter(**filter_data).annotate(
+                    user=F(type_),
+                    price_=F('USD_price'),
+                ).values('user').annotate(price=Sum('price_'))
+                if history:
+                    stat[period] = history[0].get('price')
             setattr(user_stat, types[type_], json.dumps(stat, default=float))
 
             user_stat.save()
  
-    for period, time_delta in PERIODS.items():
+    for period, time_delta in PERIOD.items():
+        users = AdvUser.objects.filter(following__date__gte=time_delta).annotate(follow_count = Count('following'))
+        for user in users:
+            user_stat = UserStat.objects.filter(user=user)
+            if not user_stat.exists():
+                user_stat = UserStat.objects.bulk_create([
+                    UserStat(network=network, user=user) for network in networks
+                ])
+            stat = getattr(user_stat[0], 'follows')
+            if isinstance(stat, str):
+                stat = json.loads(stat)
+            else:
+                stat = dict()
+            stat[period] = user.follow_count
 
-        if period != 'year':
-            users = AdvUser.objects.filter(following__date__gte=time_delta).annotate(follow_count = Count('following'))
-            for user in users:
-                user_stat = UserStat.objects.filter(user=user)
-                if not user_stat.exists():
-                    user_stat = UserStat.objects.bulk_create([
-                        UserStat(network=network, user=user) for network in networks
-                    ])
-                stat = getattr(user_stat[0], 'follows')
-                if isinstance(stat, str):
-                    stat = json.loads(stat)
-                else:
-                    stat = dict()
-                stat[period] = user.follow_count
-
-                setattr(user_stat, types[type_], json.dumps(stat, default=float))
-                user_stat.save()
+            setattr(user_stat, types[type_], json.dumps(stat, default=float))
+            user_stat.save()
 
 
 def get_top_users(type_, period, network):
