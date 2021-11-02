158e5509019cbb42f30143244ca339050f752ea1
dsahdr <72198703+dsahdr@users.noreply.github.com>
2021-11-02 13:11:20 +0300

    tron event mixins (#48)
    * tron event mixins
    
    * DRY
    
    * fix

---
+++ scaners/tron/mixins.py
@@ -0,0 +1,86 @@
+import requests
+from scaners.base import DeployData, BuyData, ApproveData, MintData
+
+
+class DeployMixin:
+    def get_events_deploy(self, last_checked_block, last_network_block):
+        type_match = {
+            'ERC721': ['fabric721_address', 'ERC721Made'],
+            'ERC1155': ['fabric1155_address', 'ERC115Made'],
+        }
+        collection_data = type_match[self.contract_type]
+        collection_address = getattr(self.network, collection_data[0])
+        event_name = collection_data[1]
+        url = self.build_tronapi_url(last_checked_block, last_network_block, collection_address, event_name)
+        events = requests.get(url).json()['data']
+        return events
+
+    def parse_data_deploy(self, event) -> DeployData:
+        return DeployData(
+            collection_name=event["args"]["name"],
+            address=self.network.wrap_in_checksum(["args"]["newToken"]),
+            deploy_block=event["blockNumber"],
+        )
+
+
+class BuyMixin:
+    def get_events_buy(self, last_checked_block, last_network_block):
+        type_match = {
+            'ERC721': ['exchange_address', 'ExchangeMadeErc721'],
+            'ERC1155': ['exchange_address', 'ExchangeMadeErc1155'],
+        }
+        collection_data = type_match[self.contract_type]
+        collection_address = getattr(self.network, collection_data[0])
+        event_name = collection_data[1]
+        url = self.build_tronapi_url(last_checked_block, last_network_block, collection_address, event_name)
+        events = requests.get(url).json()['data']
+        return events
+
+    def parse_data_buy(event) -> BuyData:
+        return BuyData(
+            buyer=event["args"]["buyer"].lower(),
+            seller=event["args"]["seller"],
+            price=event["args"]["buyAmount"],
+            amount=event["args"]["sellAmount"],
+            tx_hash=event["transactionHash"].hex(),
+            token_id=event["args"]["sellId"],
+            collection_address=event["args"]["sellTokenAddress"],
+        )
+
+
+class ApproveMixin:
+    def get_events_approve(self, last_checked_block, last_network_block):
+        collection_address = self.contract.address
+        event_name = 'Approval'
+        url = self.build_tronapi_url(last_checked_block, last_network_block, collection_address, event_name)
+        events = requests.get(url).json()['data']
+
+        return events
+
+    def parse_data_approve(event) -> ApproveData:
+        return ApproveData(
+            exchange=event["args"]["guy"],
+            user=event["args"]["src"].lower(),
+            wad=event["args"]["wad"],
+        )
+
+
+class MintMixin:
+    def get_events_mint(self, last_checked_block, last_network_block):
+        collection_address = self.contract.address
+        event_name = 'Transfer'
+        url = self.build_tronapi_url(last_checked_block, last_network_block, collection_address, event_name)
+        events = requests.get(url).json()['data']
+        return events
+
+    def parse_data_mint(event) -> MintData:
+        token_id = event["args"].get("tokenId")
+        if token_id is None:
+            token_id = event["args"].get("id")
+        return MintData(
+            token_id=token_id,
+            new_owner=event["args"]["to"].lower(),
+            old_owner=event["args"]["from"].lower(),
+            tx_hash=event["transactionHash"].hex(),
+            amount=event["args"]["value"],
+        )
--- scaners/tron/scanner.py
+++ scaners/tron/scanner.py
@@ -1,14 +1,35 @@
 from tronapi import Tron, HttpProvider
+from scaners.base import ScannerABC
+from scaners.tron.mixins import DeployMixin, ApproveMixin, BuyMixin, MintMixin
 
+class Scanner(
+    ScannerABC,
+    DeployMixin,
+    ApproveMixin,
+    BuyMixin,
+    MintMixin,
+    ):
 
-class Scanner:
-    ...
-
-    def get_last_network_block(self):
-        provider = HttpProvider(self.endpoint)
-        tron = Tron(
+    def get_tron_instance(self):
+        provider = HttpProvider(self.network.endpoint)
+        return Tron(
             full_node=provider,
             solidity_node=provider,
             event_server=provider,
         )
-        return tron.trx.get_block('latest')['block_header']['raw_data']
+
+    def build_tronapi_url(self, last_checked_block, last_network_block, collection_address, event_name):
+        last_checked_block_timestamp = self.get_block_timestamp(last_checked_block)
+        last_network_block_timestamp = self.get_block_timestamp(last_network_block)
+        url = f'{self.network.endpoint}/v1/contracts/{collection_address}/events?event_name={event_name}' \
+              f'&min_block_timestamp={last_checked_block_timestamp}' \
+              f'&max_block_timestamp={last_network_block_timestamp}'
+        return url
+
+    def get_block_timestamp(self, number):
+        tron = self.get_tron_instance()
+        return tron.trx.get_block(number)['block_header']['raw_data']['timestamp']
+
+    def get_last_network_block(self):
+        tron = self.get_tron_instance()
+        return tron.trx.get_block('latest')['number']
