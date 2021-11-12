f68d8f814ed286301a0fcc10fbce961309efa696
KagiyamaWeb <Sabitsukimaid@gmail.com>
2021-11-12 05:39:13 -0800

    Pre tron refactor tx tracker (#55)
    * transaction tracker task + tron tracker
    
    * removed old tx_tracker
    
    * fixed and testen tron tx tracker
    
    * Removed extra end line
    
    Co-authored-by: dsahdr <72198703+dsahdr@users.noreply.github.com>

--- src/store/tasks.py
+++ src/store/tasks.py
@@ -1,8 +1,12 @@
 from datetime import datetime, timedelta
 
 from celery import shared_task
-from src.store.models import Bid, Status, Token
+from src.store.models import Bid, Status, Token, TransactionTracker
+from src.networks.models import Types
 from src.store.services.auction import end_auction
+from web3.exceptions import TransactionNotFound
+from tronapi import HttpProvider, Tron
+from src.settings import config
 
 
 @shared_task(name="remove_pending_tokens")
@@ -54,3 +58,46 @@ def incorrect_bid_checker():
         if user_balance < bid.amount * bid.quantity or allowance < bid.amount * bid.quantity:
             bid.state = Status.EXPIRED
             bid.save()
+
+def check_ethereum_transactions(tx):
+    w3 = tx.token.collection.network.get_web3_connection()
+    try:
+        transaction = w3.eth.getTransactionReceipt(tx.tx_hash)
+        print(f"Transaction status success - {bool(transaction.get('status'))}")
+        return bool(transaction.get('status'))
+
+    except TransactionNotFound:
+        print("Transaction not yet mined")
+        return "not found"
+
+def check_tron_transactions(tx):
+    provider = HttpProvider(tx.token.collection.network.endpoint)
+    tron = Tron(
+            full_node=provider,
+            solidity_node=provider,
+            event_server=provider,
+            private_key=config.PRIV_KEY,
+        )
+    try:
+        transaction = tron.trx.get_transaction(tx.tx_hash)
+        print(f"Transaction status success - {transaction['ret'][0]['contractRet']}")
+        return transaction['ret'][0]['contractRet']
+
+    except ValueError:
+        print("Transaction not yet mined")
+        return "not found"
+
+@shared_task(name="transaction_tracker")
+def transaction_tracker():
+    for type in Types._member_names_:
+        tx_list = TransactionTracker.objects.filter(token__collection__network__network_type=type)
+        for tx in tx_list:
+            if type == 'ethereum':
+                tx = check_ethereum_transactions(tx)
+            elif type == 'tron':
+                tx = check_tron_transactions(tx)
+            if tx != "not found":
+                if tx == 0 or tx == 'REVERT':
+                    tx.item.selling = True
+                    tx.item.save()
+                tx.delete()
