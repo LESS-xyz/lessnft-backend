68e58bfe7345fcae13dcc33f171f5f6afc96ea23
Vladimir Kuratov <kuratovv29@gmail.com>
2021-11-26 13:28:17 +0300

    Update test_utilities.py

--- src/tests/test_utilities.py
+++ src/tests/test_utilities.py
@@ -40,16 +40,19 @@ def test_paginate_mixin():
     assert response['total_pages'] == 6
     assert response['total'] == 58
 
+    # test negative value and page not equal 1
     response = PaginateMixin().paginate(Request({"page": 6, "items_per_page": -10}), ITEMS)
     assert response['results_per_page'] == 10
     assert response['results'][0] == 'item_50'
     assert response['results'][-1] == 'item_57'
 
+    # test zero values
     response = PaginateMixin().paginate(Request({"page": 0, "items_per_page": 0}), ITEMS)
     assert response['results_per_page'] == 50
     assert response['results'][0] == 'item_0'
     assert response['results'][-1] == 'item_49'
 
+    # test incorrect values type
     response = PaginateMixin().paginate(Request({"page": [1], "items_per_page": [10]}), ITEMS)
     assert response['results_per_page'] == 50
     assert response['results'][0] == 'item_0'
