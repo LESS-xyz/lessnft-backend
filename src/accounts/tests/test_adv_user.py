ba1a8a7a9a067bfec64cd3378b6463765785bc11
Kagiyama <sabitsukimaid@gmail.com>
2021-11-29 15:31:32 +0300

    Additional tests

--- src/accounts/tests/test_adv_user.py
+++ src/accounts/tests/test_adv_user.py
@@ -1,16 +1,20 @@
 import pytest
 
 from src.accounts.models import AdvUser
-
+from django.core.exceptions import ObjectDoesNotExist
 
 @pytest.mark.django_db
 def test_adv_user_manager(mixer):
-    mixer.blend(
+    adv_user = mixer.blend(
         'accounts.AdvUser', 
         display_name='testuser',
         custom_url='testurl'
     )
 
     assert AdvUser.objects.get_by_custom_url('testurl').display_name == 'testuser'
-    assert AdvUser.objects.get_by_custom_url(1).display_name == 'testuser'
-    assert AdvUser.objects.get_by_custom_url('1').display_name == 'testuser'
+    assert AdvUser.objects.get_by_custom_url(adv_user.id).display_name == 'testuser'
+    assert AdvUser.objects.get_by_custom_url(str(adv_user.id)).display_name == 'testuser'
+    try:
+        AdvUser.objects.get_by_custom_url('Fake')
+    except ObjectDoesNotExist:
+        assert True
--- src/store/tests/test_store_managers.py
+++ src/store/tests/test_store_managers.py
@@ -1,28 +1,20 @@
 import pytest
 from src.store.models import Status, Collection, Token
+from django.core.exceptions import ObjectDoesNotExist
 
 
 @pytest.mark.django_db
 def test_check_commited(mixer):
-    network_eth = mixer.blend(
-        'networks.Network',
-        name = 'Ethereum',
-    )
-    network_polygon= mixer.blend(
-        'networks.Network',
-        name = 'Polygon',
-    )
-
     mixer.blend(
         "store.Collection",
         name='expired_collection',
         status=Status.EXPIRED,
-        network=network_eth,
+        network__name='Ethereum',
     )
     mixer.cycle(5).blend(
         "store.Collection",
         status=Status.COMMITTED,
-        network=network_eth,
+        network__name='Ethereum'
     )
     mixer.cycle(7).blend(
         "store.Collection",
@@ -32,7 +24,7 @@ def test_check_commited(mixer):
     mixer.blend(
         "store.Collection",
         status=Status.COMMITTED,
-        network=network_polygon,
+        network__name='Polygon'
     )
 
     '''Checking "committed" collections querry set'''
@@ -84,37 +76,35 @@ def test_check_user_collections(mixer):
     assert len(Collection.objects.user_collections(user=user)) == 15
     assert len(Collection.objects.user_collections(user=second_user)) == 11
     assert len(Collection.objects.user_collections(user=second_user, network=network_polygon)) == 1
+    assert not Collection.objects.user_collections(user=None, network=network_polygon)
 
 
 @pytest.mark.django_db
 def test_collections_by_short_url(mixer):
-    network_polygon = mixer.blend(
-        'networks.Network',
-        name = 'Polygon',
-    )
-    network_eth = mixer.blend(
-        'networks.Network',
-        name = 'Ethereum',
-    )
-
     polygon_collection = mixer.blend(
         "store.Collection",
         name = 'test_collection',
         status = Status.COMMITTED,
         short_url = 'testurl',
-        network = network_polygon 
+        network__name='Polygon'
     )
     eth_collection = mixer.blend(
         "store.Collection",
         name = 'test_collection_2',
         status = Status.COMMITTED,
         short_url = 'testurl2',
-        network = network_eth 
+        network__name='Ethereum'
     )
 
     '''Checking getting collection by short_url and id'''
     assert Collection.objects.get_by_short_url('testurl').name == 'test_collection'
+    assert not Collection.objects.get_by_short_url('testurl').name == 'test_collection_2'
     assert Collection.objects.get_by_short_url('testurl2').name == 'test_collection_2'
+    try:
+        Collection.objects.get_by_short_url('testurl3')
+    except ObjectDoesNotExist:
+        assert True
+
 
     assert Collection.objects.get_by_short_url(polygon_collection.id).name == 'test_collection'
     assert Collection.objects.get_by_short_url(str(eth_collection.id)).name == 'test_collection_2'
@@ -123,25 +113,16 @@ def test_collections_by_short_url(mixer):
 
 @pytest.mark.django_db
 def test_check_collections_by_network(mixer):
-    network_eth = mixer.blend(
-        'networks.Network',
-        name = 'Ethereum',
-    )
-    network_polygon= mixer.blend(
-        'networks.Network',
-        name = 'Polygon',
-    )
-
     mixer.blend(
         "store.Collection",
         name='expired_collection',
         status=Status.EXPIRED,
-        network=network_eth,
+        network__name='Ethereum'
     )
     mixer.cycle(5).blend(
         "store.Collection",
         status=Status.COMMITTED,
-        network=network_eth,
+        network__name='Ethereum'
     )
     mixer.cycle(3).blend(
         "store.Collection",
@@ -151,61 +132,69 @@ def test_check_collections_by_network(mixer):
     mixer.blend(
         "store.Collection",
         status=Status.COMMITTED,
-        network=network_polygon,
+        network__name='Polygon'
     )
 
     '''Checking Collections by network'''
     assert len(Collection.objects.network('Polygon')) == 1
     assert len(Collection.objects.network('Ethereum')) == 6
     assert len(Collection.objects.network(None)) == 10
+    assert not Collection.objects.network('Fake').exists()
+    assert not Collection.objects.network(['Ethereum']).exists()
 
 
 @pytest.mark.django_db
 def test_check_hot_collections(mixer):
-    network_eth = mixer.blend(
-        'networks.Network',
-        name = 'Ethereum',
-    )
-
     collection = mixer.blend(
         "store.Collection",
         name='expired_collection',
         status=Status.COMMITTED,
-        network=network_eth,
+        network__name='Ethereum'
+    )
+    default_collection = mixer.blend(
+        "store.Collection",
+        status=Status.COMMITTED,
+        is_default=True
+    )
+    pending_collection = mixer.blend(
+        "store.Collection",
+        status=Status.PENDING,
+        network__name='Ethereum'
+    )
+    mixer.blend(
+        "store.Collection",
+        status=Status.COMMITTED,
+        network__name='Ethereum'
     )
 
     mixer.cycle(5).blend(
         'store.Token', 
         collection=collection, 
-        status=Status.EXPIRED
+        status=Status.PENDING
     )
     mixer.cycle(3).blend('store.Token', collection=collection, status=Status.COMMITTED)
+    mixer.cycle(3).blend('store.Token', collection=default_collection, status=Status.COMMITTED)
+    mixer.cycle(3).blend('store.Token', collection=pending_collection, status=Status.COMMITTED)
 
     '''Checking Hot Collections (non-default collections with committed tokens)'''
-    assert len(Collection.objects.hot_collections()) == 1
+    assert len(Collection.objects.hot_collections()) == 2
+    assert not Collection.objects.hot_collections() == 0
+    assert all([c.status==Status.COMMITTED or c.status==Status.PENDING for c in Collection.objects.hot_collections()])
 
 
 @pytest.mark.django_db
 def test_token_manager(mixer):
-    network = mixer.blend(
-        'networks.Network',
-        name = 'Ethereum',
-    )
-    network_polygon = mixer.blend(
-        'networks.Network',
-        name = 'Polygon',
-    )
     collection_eth = mixer.blend(
         "store.Collection",
         name = 'test_collection',
         status = Status.COMMITTED,
-        network = network
+        network__name='Ethereum' 
     )
     collection_polygon = mixer.blend(
         "store.Collection",
         name = 'test_collection',
         status = Status.COMMITTED,
-        network = network_polygon
+        network__name='Polygon' 
     )
     mixer.cycle(3).blend(
         'store.Token',
@@ -231,3 +220,6 @@ def test_token_manager(mixer):
     '''Checking Tokens by network'''
     assert len(Token.objects.network('Polygon')) == 4
     assert len(Token.objects.network('Ethereum')) == 3
+    assert len(Token.objects.network(None)) == 7
+    assert not Token.objects.network('Fake')
+    assert not Token.objects.network(['Polygon'])
